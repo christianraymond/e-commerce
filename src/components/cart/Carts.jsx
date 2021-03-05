5a32b89653ca9308d0a418769cd074b26c412e6b
aditechsoft <christian.ngubana@platform45.com>
2021-03-05 16:58:16 +0200

    Can add, increase, decrease and remove item from checkout

--- src/components/cart/Carts.jsx
+++ src/components/cart/Carts.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container, Typography, Button, Grid, CardContent } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import useStyle from './styles';
 
 import CartItem from './cartItem/CartItem';
 
-const Carts = ({ cart }) => {
+const Carts = ({ cart, handleCartQty, handleRemoveFromCart, handleEmptyCart }) => {
 
     const classes = useStyle();
     
@@ -13,7 +14,9 @@ const Carts = ({ cart }) => {
     const isEmpty = !cart.line_items;
 
       const EmptyCart = () => ( 
-        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+        <Typography variant="subtitle1">You have no items in your shopping cart,
+        <Link to="/" className={classes.link}> start adding some</Link>!
+        </Typography>
       )
   
       const FilledCart = () => ( 
@@ -21,14 +24,14 @@ const Carts = ({ cart }) => {
         <Grid container spacing={3}>
           {cart.line_items.map((lineItem) => (
             <Grid item xs={12} sm={4} key={lineItem.id}>
-              <CartItem lineItem={lineItem}/>
+              <CartItem lineItem={lineItem} onHandleCartQty={handleCartQty} onHandleRemoveFromCart={handleRemoveFromCart}/>
             </Grid>
           ))}
         </Grid>
         <div className={classes.cardDetails}>
           <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
           <div>
-            <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty cart</Button>
+            <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
             <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
           </div>
         </div>

9d5cdfd94bd3051ffbf645125cf57b0645d74124
aditechsoft <christian.ngubana@platform45.com>
2021-03-07 23:35:51 +0200

    set shipping countries and subdivision dynamically

--- src/components/checkoutForm/Checkout/Checkout.jsx
+++ src/components/checkoutForm/Checkout/Checkout.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
 
+import { commerce }   from '../../../lib/commerce';
+
 import useStyles from './styles';
 const steps = ['Shipping address', 'Payment details'];
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
  const [ activeStep, setActiveStep ] = useState(0);
+ const [ checkoutToken, setCheckoutToken ] = useState(null)
  const classes = useStyles();
+
+ useEffect(() => {
+     const generateToken = async () => {
+         try {
+             const token = await commerce.checkout.generateToken(cart.id, { type: 'cart'});
+             setCheckoutToken(token);
+             console.log(token)
+             
+         } catch (error) {
+             
+         }
+     }
+     generateToken()
+ }, []);
 
  const Confirmation = () => (
      <div>COnfirmation!</div>
  )
- const Form = () => activeStep === 0 ? <AddressForm/> : <PaymentForm/>
+ const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken}/> : <PaymentForm/>
 
     return (
         <>
@@ -29,7 +46,7 @@ const Checkout = () => {
                           </Step>
                       ))}
                   </Stepper>
-                  { activeStep === steps.length ? <Confirmation /> : <Form/>}
+                  { activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form/>}
               </Paper>
           </main>  
         </>
